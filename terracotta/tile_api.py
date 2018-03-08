cedf9c14127b3c4806308c62f39cacd6acc381d4
Philip Graae <mrpgraae@gmail.com>
2018-03-08 18:22:14 +0100

    Contrast stretch and tilestore

--- terracotta/tile_api.py
+++ terracotta/tile_api.py
@@ -3,21 +3,27 @@ from io import BytesIO
 from flask import Blueprint, current_app, abort, send_file
 import numpy as np
 
-from terracotta.app import tilestore
+import terracotta
 from terracotta.tile import TileNotFoundError, TileOutOfBoundsError
 
 
 tile_api = Blueprint('tile_api', __name__)
+tilestore = None
 
 
-@tile_api.route('/<dataset>/<int:tile_z>/<int:tile_x>/<int:tile_y>.png', methods=['GET'])
-@tile_api.route('/<dataset>/<timestep>/<int:tile_z>/<int:tile_x>/<int:tile_y>.png',
+def init_tilestore(datasets, options):
+    global tilestore
+    tilestore = terracotta.tile.TileStore(datasets, options['max_cache_size'])
+
+
+@tile_api.route('/terracotta/<dataset>/<int:tile_z>/<int:tile_x>/<int:tile_y>.png', methods=['GET'])
+@tile_api.route('/terracotta/<dataset>/<timestep>/<int:tile_z>/<int:tile_x>/<int:tile_y>.png',
                 methods=['GET'])
 def get_tile(dataset, tile_z, tile_x, tile_y, timestep=None):
     """Respond to tile requests"""
 
     try:
-        img = tilestore.tile(tile_x, tile_y, tile_z, dataset, timestep)
+        img = tilestore.tile(tile_x, tile_y, tile_z, dataset, timestep, contrast_stretch=True)
     except TileNotFoundError:
         if current_app.debug:
             raise
