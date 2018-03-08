b8dec2c53232ddb866158ba4bd896e582aa52600
Philip Graae <mrpgraae@gmail.com>
2018-03-08 18:20:44 +0100

    move tilestore to tile_api

--- terracotta/app.py
+++ terracotta/app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 
+import terracotta
 import terracotta.config as config
 import terracotta.tile as tile
-
-
-tilestore = None
 
 
 def create_app(cfg_file, debug=False):
@@ -12,11 +10,12 @@ def create_app(cfg_file, debug=False):
     from terracotta.tile_api import tile_api
     global tilestore
 
+    options, datasets = config.parse_cfg(cfg_file)
+    terracotta.tile_api.init_tilestore(datasets, options)
+
     new_app = Flask('terracotta')
     new_app.debug = debug
     new_app.register_blueprint(tile_api)
-    options, datasets = config.parse_cfg(cfg_file)
-    tilestore = tile.TileStore(datasets, options['max_cache_size'])
 
     return new_app
 
