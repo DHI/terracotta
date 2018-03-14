d17b4b5511fc6ce81812bcf38ada02450eec619b
Philip Graae <mrpgraae@gmail.com>
2018-03-14 17:51:06 +0100

    Add Versioneer

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
+import versioneer
+
 from setuptools import setup, find_packages
 
 
 setup(
     name='terracotta',
-    version='0.1.0',
+    version=versioneer.get_version(),
+    cmdclass=verioneer.get_cmdclass()
     description='An XYZ tile server written in Python',
     author='Philip Graae',
     author_email='phgr@dhigroup.com',
