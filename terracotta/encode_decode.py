c094137be218247619b9146655e79c25edc0909b
Philip Graae <mrpgraae@gmail.com>
2018-03-16 15:24:43 +0100

    Make array_to_img work with all kinds of data

--- terracotta/encode_decode.py
+++ terracotta/encode_decode.py
@@ -1,33 +1,53 @@
 import numpy as np
+import matplotlib
+import matplotlib.cm as cm
 from PIL import Image
 
 
-def array_to_img(arr, alpha_mask):
-    """Convert Numpy array to png img.
-    Only single-band uint8 data supported for now.
+def array_to_img(arr, alpha_mask=None):
+    """Convert Numpy array to img.
+    input array can be shape (H,W), (H,W,2), (H,W,3) or (H,W,4).
+    Will be interpreted as L, LA, RGB and RGBA respectively.
 
     Parameters
     ----------
     arr: numpy array
-        Image greyscale data.
+        Image data.
     alpha_mask: numpy array
         Alpha values (0 transparent, 255 opaque).
+        If input shape is (H,W,2) or (H,W,4) and alpha_mask is None,
+        the last slice will be used as alpha.
 
     Returns
     -------
-    out: PIL Image
-        greyscale with alpha image"""
+    out: PIL Image"""
 
-    assert arr.ndim == 2
+    if not arr.ndim > 1 and arr.ndim < 4:
+        raise ValueError("Img must have 2 or 3 dimensions")
+    if arr.ndim == 2:
+        # Make a new dimension for alpha mask
+        arr = arr[:, :, np.newaxis]
+    elif arr.ndim == 3 and arr.shape[2] > 4:
+        raise ValueError("Check array shape, only L, LA, RGB and RGBA supported")
+    if alpha_mask is None:
+        if arr.shape[2] == 2 or arr.shape[2] == 4:
+            # Assume last slice is alpha
+            alpha_mask = None
+        else:
+            alpha_mask = np.full((arr.shape[0], arr.shape[1]), 255, dtype=np.uint8)
 
-    arr = arr.astype(np.uint8)
-    alpha_mask = alpha_mask.astype(np.uint16)
+    if arr.shape[2] == 1 or arr.shape[2] == 3:
+        arr = np.dstack((arr, alpha_mask))
+    else:
+        if alpha_mask is not None:
+            arr[:, :, -1] = alpha_mask
 
-    # High byte is alpha, low is greyscale val
-    alpha_mask <<= 8
-    alpha_mask += arr
-
-    img = Image.fromarray(alpha_mask, mode='LA')
+    if arr.shape[2] == 2:
+        img = Image.fromarray(arr, mode='LA')
+    elif arr.shape[2] == 4:
+        img = Image.fromarray(arr, mode='RGBA')
+    else:
+        raise ValueError("Unreachable code")
 
     return img
 
