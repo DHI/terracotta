745016ab3d68118598ea57cfe597cacf841731a8
Philip Graae <mrpgraae@gmail.com>
2018-03-21 15:28:14 +0100

    Expand ~ to user home on Windows

--- terracotta/cli.py
+++ terracotta/cli.py
@@ -1,12 +1,22 @@
+import os
+
 import click
 
 import terracotta.app as app
 
 
+class ExpandedPath(click.Path):
+    def convert(self, value, *args, **kwargs):
+        if os.name == 'nt':
+            # Only expand on Windows, as shell will do it on *nix
+            value = os.path.expanduser(value)
+        return super().convert(value, *args, **kwargs)
+
+
 @click.command()
 @click.option('--debug', is_flag=True, default=False,
               help='Enable Flask debugging')
-@click.option('--cfg-file', type=click.Path(exists=True), default='./config.cfg')
+@click.option('--cfg-file', type=ExpandedPath(exists=True), default='./config.cfg')
 def cli(debug, cfg_file):
     """Entry point of terracotta CLI"""
     app.run_app(cfg_file, debug)
