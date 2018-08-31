0f5e8f607c5ba1971c7ec51ef46603610403e390
Dion Häfner <dimh@dhigroup.com>
2018-08-31 10:54:11 +0200

    add test for chunked metadata

--- tests/drivers/test_staticmethods.py
+++ tests/drivers/test_staticmethods.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def test_safe_transform_oom():
+def test_default_transform():
     from rasterio.warp import calculate_default_transform
     from rasterio._err import CPLE_OutOfMemoryError
 
@@ -15,6 +15,7 @@ def test_safe_transform_oom():
         -10, -10, 10, 10
     )
 
+    # we can handle cases rasterio can't
     with pytest.raises(CPLE_OutOfMemoryError):
         rio_transform, rio_width, rio_height = calculate_default_transform(*args)
 
@@ -24,4 +25,26 @@ def test_safe_transform_oom():
     assert our_height == args[3]
 
 
+@pytest.mark.parametrize('use_chunks', [True, False])
+def test_compute_metadata(big_raster_file, use_chunks):
+    import rasterio
+    import numpy as np
 
+    from terracotta.drivers.raster_base import RasterDriver
+
+    with rasterio.open(str(big_raster_file)) as src:
+        data = src.read(1)
+        data = data[np.isfinite(data) & (data != src.nodata)]
+
+    mtd = RasterDriver.compute_metadata(str(big_raster_file), use_chunks=use_chunks)
+
+    np.testing.assert_allclose(mtd['range'], (data.min(), data.max()))
+    np.testing.assert_allclose(mtd['mean'], data.mean())
+    np.testing.assert_allclose(mtd['stdev'], data.std())
+
+    # allow error of 1%
+    np.testing.assert_allclose(
+        mtd['percentiles'], 
+        np.percentile(data, np.arange(1, 100)),
+        rtol=0.01
+    )
