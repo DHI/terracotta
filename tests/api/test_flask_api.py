9c81a582f6b02f43bdd6170497ccf411bd605ade
Dion Häfner <dimh@dhigroup.com>
2018-09-16 12:24:10 +0200

    yea i'm stupid

--- tests/api/test_flask_api.py
+++ tests/api/test_flask_api.py
@@ -108,9 +108,7 @@ def test_get_singleband_explicit_cmap(client, use_read_only_database, raster_fil
 
     rv = client.get(f'/singleband/val11/val12/{z}/{x}/{y}.png?colormap=explicit'
                     f'&explicit_color_map={urlsafe_json(explicit_cmap)}')
-    print(urlsafe_json(explicit_cmap))
     assert rv.status_code == 200, rv.data.decode('utf-8')
-    assert False
 
     img = Image.open(BytesIO(rv.data))
     assert np.asarray(img).shape == settings.TILE_SIZE
