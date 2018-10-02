57cb582946d390cd2a25362f7c0384d2110d9227
Dion Häfner <dimh@dhigroup.com>
2018-10-02 11:24:13 +0200

    test warning on unoptimized gtiff

--- tests/drivers/test_staticmethods.py
+++ tests/drivers/test_staticmethods.py
@@ -1,5 +1,10 @@
 import pytest
 
+import rasterio
+import rasterio.features
+from shapely.geometry import shape, MultiPolygon
+import numpy as np
+
 
 def test_default_transform():
     from rasterio.warp import calculate_default_transform
@@ -14,7 +19,7 @@ def test_default_transform():
         -10, -10, 10, 10
     )
 
-    # GDAL defaults don't round-trip
+    # GDAL defaults don't round-trip non-square pixels
     _, rio_width, rio_height = calculate_default_transform(*args)
     assert rio_width != args[2]
     assert rio_height != args[3]
@@ -32,11 +37,6 @@ def geometry_mismatch(shape1, shape2):
 
 @pytest.mark.parametrize('use_chunks', [True, False])
 def test_compute_metadata(big_raster_file, use_chunks):
-    import rasterio
-    import rasterio.features
-    from shapely.geometry import shape, MultiPolygon
-    import numpy as np
-
     from terracotta.drivers.raster_base import RasterDriver
 
     with rasterio.open(str(big_raster_file)) as src:
@@ -60,7 +60,7 @@ def test_compute_metadata(big_raster_file, use_chunks):
     np.testing.assert_allclose(
         mtd['percentiles'],
         np.percentile(valid_data, np.arange(1, 100)),
-        rtol=0.02
+        rtol=1e-2
     )
 
     assert geometry_mismatch(shape(mtd['convex_hull']), convex_hull) < 1e-8
@@ -74,12 +74,7 @@ def test_compute_metadata_invalid(invalid_raster_file, use_chunks):
         RasterDriver.compute_metadata(str(invalid_raster_file), use_chunks=use_chunks)
 
 
-def test_compute_metadata_nocrick(big_raster_file):
-    import rasterio
-    import rasterio.features
-    from shapely.geometry import shape, MultiPolygon
-    import numpy as np
-
+def test_compute_metadata_nocrick(big_raster_file, monkeypatch):
     with rasterio.open(str(big_raster_file)) as src:
         data = src.read(1)
         valid_data = data[np.isfinite(data) & (data != src.nodata)]
@@ -89,28 +84,59 @@ def test_compute_metadata_nocrick(big_raster_file):
 
     convex_hull = MultiPolygon([shape(s['geometry']) for s in dataset_shape]).convex_hull
 
+    from terracotta import exceptions
     import terracotta.drivers.raster_base
-    try:
-        terracotta.drivers.raster_base.has_crick = False
 
-        with pytest.warns(UserWarning):
+    with monkeypatch.context() as m:
+        m.setattr(terracotta.drivers.raster_base, 'has_crick', False)
+
+        with pytest.warns(exceptions.PerformanceWarning):
             mtd = terracotta.drivers.raster_base.RasterDriver.compute_metadata(
                 str(big_raster_file), use_chunks=True)
 
-        # compare
-        np.testing.assert_allclose(mtd['valid_percentage'], 100 * valid_data.size / data.size)
-        np.testing.assert_allclose(mtd['range'], (valid_data.min(), valid_data.max()))
-        np.testing.assert_allclose(mtd['mean'], valid_data.mean())
-        np.testing.assert_allclose(mtd['stdev'], valid_data.std())
+    # compare
+    np.testing.assert_allclose(mtd['valid_percentage'], 100 * valid_data.size / data.size)
+    np.testing.assert_allclose(mtd['range'], (valid_data.min(), valid_data.max()))
+    np.testing.assert_allclose(mtd['mean'], valid_data.mean())
+    np.testing.assert_allclose(mtd['stdev'], valid_data.std())
 
-        # allow error of 1%, since we only compute approximate quantiles
-        np.testing.assert_allclose(
-            mtd['percentiles'],
-            np.percentile(valid_data, np.arange(1, 100)),
-            rtol=0.01
-        )
+    # allow error of 1%, since we only compute approximate quantiles
+    np.testing.assert_allclose(
+        mtd['percentiles'],
+        np.percentile(valid_data, np.arange(1, 100)),
+        rtol=0.01
+    )
 
-        assert geometry_mismatch(shape(mtd['convex_hull']), convex_hull) < 1e-8
+    assert geometry_mismatch(shape(mtd['convex_hull']), convex_hull) < 1e-8
 
-    finally:
-        terracotta.drivers.raster_base.has_crick = True
+
+def test_compute_metadata_unoptimized(unoptimized_raster_file):
+    from terracotta import exceptions
+    from terracotta.drivers.raster_base import RasterDriver
+
+    with rasterio.open(str(unoptimized_raster_file)) as src:
+        data = src.read(1)
+        valid_data = data[np.isfinite(data) & (data != src.nodata)]
+        dataset_shape = list(rasterio.features.dataset_features(
+            src, bidx=1, as_mask=True, geographic=True
+        ))
+
+    convex_hull = MultiPolygon([shape(s['geometry']) for s in dataset_shape]).convex_hull
+
+    # compare
+    with pytest.warns(exceptions.PerformanceWarning):
+        mtd = RasterDriver.compute_metadata(str(unoptimized_raster_file))
+
+    np.testing.assert_allclose(mtd['valid_percentage'], 100 * valid_data.size / data.size)
+    np.testing.assert_allclose(mtd['range'], (valid_data.min(), valid_data.max()))
+    np.testing.assert_allclose(mtd['mean'], valid_data.mean())
+    np.testing.assert_allclose(mtd['stdev'], valid_data.std())
+
+    # allow some error margin since we only compute approximate quantiles
+    np.testing.assert_allclose(
+        mtd['percentiles'],
+        np.percentile(valid_data, np.arange(1, 100)),
+        rtol=1e-2
+    )
+
+    assert geometry_mismatch(shape(mtd['convex_hull']), convex_hull) < 1e-8
