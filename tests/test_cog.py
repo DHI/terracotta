5a454a9a3dde18ffc241e9a16c304fc9d70ffbce
Dion Häfner <dimh@dhigroup.com>
2018-10-04 11:15:05 +0200

    test cog validator more

--- tests/test_cog.py
+++ tests/test_cog.py
@@ -1,7 +1,8 @@
+import pytest
+
 import affine
 import rasterio
 from rasterio.shutil import copy
-from rasterio.io import MemoryFile
 from rasterio.enums import Resampling
 
 import numpy as np
@@ -34,7 +35,7 @@ def test_validate_optimized(tmpdir):
         blockysize=256
     )
 
-    with MemoryFile() as memfile, memfile.open(**profile) as dst:
+    with rasterio.open(outfile, 'w', **profile) as dst:
         dst.write(raster_data, 1)
 
         overviews = [2 ** j for j in range(1, 4)]
@@ -123,27 +124,6 @@ def test_validate_not_tiled(tmpdir):
     assert not cog.validate(outfile)
 
 
-def test_validate_not_sorted(tmpdir):
-    from terracotta import cog
-
-    outfile = str(tmpdir / 'raster.tif')
-    raster_data = 1000 * np.random.rand(512, 512).astype(np.uint16)
-
-    profile = BASE_PROFILE.copy()
-    profile.update(
-        height=raster_data.shape[0],
-        width=raster_data.shape[1]
-    )
-
-    with rasterio.open(outfile, 'w', **profile) as dst:
-        dst.write(raster_data, 1)
-
-        overviews = [2 ** j for j in [4, 2, 1, 3]]
-        dst.build_overviews(overviews, Resampling.nearest)
-
-    assert not cog.validate(outfile)
-
-
 def test_validate_wrong_offset(tmpdir):
     from terracotta import cog
 
@@ -164,5 +144,55 @@ def test_validate_wrong_offset(tmpdir):
 
         overviews = [2 ** j for j in range(1, 4)]
         dst.build_overviews(overviews, Resampling.nearest)
+
+    assert not cog.validate(outfile)
+
+
+def test_validate_external_overview(tmpdir):
+    import os
+    from terracotta import cog
+
+    outfile = str(tmpdir / 'raster.tif')
+    raster_data = 1000 * np.random.rand(512, 512).astype(np.uint16)
+
+    profile = BASE_PROFILE.copy()
+    profile.update(
+        height=raster_data.shape[0],
+        width=raster_data.shape[1],
+        tiled=True,
+        blockxsize=256,
+        blockysize=256
+    )
+
+    with rasterio.Env(TIFF_USE_OVR=True):
+        with rasterio.open(outfile, 'w', **profile) as dst:
+            dst.write(raster_data, 1)
+
+            overviews = [2 ** j for j in range(1, 4)]
+            dst.build_overviews(overviews, Resampling.nearest)
+
+            copy(dst, outfile, copy_src_overviews=False, **profile)
+
+        assert os.path.isfile(f'{outfile}.ovr')
+
+    assert not cog.validate(outfile)
+
+
+@pytest.mark.filterwarnings("ignore:Dataset has no geotransform set")
+def test_validate_not_gtiff(tmpdir):
+    from terracotta import cog
+
+    outfile = str(tmpdir / 'raster.png')
+    raster_data = 1000 * np.random.rand(512, 512).astype(np.uint16)
+
+    profile = BASE_PROFILE.copy()
+    profile.update(
+        height=raster_data.shape[0],
+        width=raster_data.shape[1],
+        driver='PNG'
+    )
+
+    with rasterio.open(outfile, 'w', **profile) as dst:
+        dst.write(raster_data, 1)
 
     assert not cog.validate(outfile)
