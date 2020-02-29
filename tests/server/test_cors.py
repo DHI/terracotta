757b8769d6a281fd176343f643916dcdc6bd1760
j08lue <j08lue@gmail.com>
2020-02-29 15:49:22 +0100

    workaround for sticky state

--- tests/server/test_cors.py
+++ tests/server/test_cors.py
@@ -1,27 +1,20 @@
 import pytest
 
 
-@pytest.fixture(scope='module')
-def client_defaults():
-    from terracotta.server import create_app
-    flask_app = create_app()
-    with flask_app.test_client() as client:
-        yield client
-
-
-@pytest.fixture(scope='module')
-def client_metadata_disallowed():
+@pytest.fixture(scope='function')
+def client_all_disallowed():
     from terracotta.server import create_app
     import terracotta
     terracotta.update_settings(
-        ALLOWED_ORIGINS_METADATA=''
+        ALLOWED_ORIGINS_METADATA='',
+        ALLOWED_ORIGINS_TILES=''
     )
     flask_app = create_app()
     with flask_app.test_client() as client:
         yield client
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='function')
 def client_all_allowed():
     from terracotta.server import create_app
     import terracotta
@@ -34,25 +27,25 @@ def client_all_allowed():
         yield client
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture
 def valid_metadata_path():
     return '/metadata/val11/x/val12/'
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture
 def valid_singleband_path(raster_file_xyz):
     x, y, z = raster_file_xyz
     return f'/singleband/val11/x/val12/{z}/{x}/{y}.png'
 
 
-def test_cors_defaults(
-    client_defaults, use_testdb, valid_metadata_path, valid_singleband_path
+def test_cors_all_disallowed(
+        client_all_disallowed, use_testdb, valid_metadata_path, valid_singleband_path
 ):
-    rv = client_defaults.get(valid_metadata_path)
+    rv = client_all_disallowed.get(valid_metadata_path)
     assert rv.status_code == 200
-    assert 'Access-Control-Allow-Origin' in rv.headers
+    assert 'Access-Control-Allow-Origin' not in rv.headers
 
-    rv = client_defaults.get(valid_singleband_path)
+    rv = client_all_disallowed.get(valid_singleband_path)
     assert rv.status_code == 200
     assert 'Access-Control-Allow-Origin' not in rv.headers
 
@@ -67,9 +60,3 @@ def test_cors_all_allowed(
     rv = client_all_allowed.get(valid_singleband_path)
     assert rv.status_code == 200
     assert 'Access-Control-Allow-Origin' in rv.headers
-
-
-def test_cors_metadata_disallowed(client_metadata_disallowed, use_testdb, valid_metadata_path):
-    rv = client_metadata_disallowed.get(valid_metadata_path)
-    assert rv.status_code == 200
-    assert 'Access-Control-Allow-Origin' not in rv.headers
