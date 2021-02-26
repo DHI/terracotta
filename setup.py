1e8d3362eeb331679a95596e3fb0222e98c0bcba
Dion Häfner <dion.haefner@nbi.ku.dk>
2021-02-26 12:55:34 +0100

    pin rasterio version

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
         'pillow',
         'pyyaml>=3.10',  # downstream dependency of apispec
         'shapely',
-        'rasterio>=1.0',
+        'rasterio>=1.0,<=1.1.8',  # TODO: unpin when performance issues with GDAL3 are fixed
         'shapely',
         'toml',
         'tqdm'
