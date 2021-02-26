48a35c8e14ea0f4496303e5753a8c3c232645133
Dion Häfner <dion.haefner@nbi.ku.dk>
2021-02-26 13:05:07 +0100

    don't import mock_xray_client into global score

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -2,48 +2,58 @@ import pytest
 
 import time
 
-from moto import mock_xray_client, XRaySegment
-
 
 @pytest.mark.filterwarnings('ignore:pytest.PytestUnraisableExceptionWarning')
-@mock_xray_client
 def test_xray_tracing(caplog):
-    from terracotta import update_settings
-    import terracotta.profile
+    from moto import mock_xray_client, XRaySegment
+    # use another closure so mock_xray_client isn't in global scope
 
-    update_settings(XRAY_PROFILE=True)
+    @mock_xray_client
+    def run_test():
+        from terracotta import update_settings
+        import terracotta.profile
 
-    @terracotta.profile.trace('dummy')
-    def func_to_trace():
-        time.sleep(0.1)
+        update_settings(XRAY_PROFILE=True)
 
-    with XRaySegment():
-        func_to_trace()
-
-    with XRaySegment():
-        with terracotta.profile.trace('dummy2'):
+        @terracotta.profile.trace('dummy')
+        def func_to_trace():
             time.sleep(0.1)
 
-    for record in caplog.records:
-        assert record.levelname != 'ERROR'
+        with XRaySegment():
+            func_to_trace()
 
-    # sanity check, recording without starting a segment should fail
-    func_to_trace()
-    assert any('cannot find the current segment' in rec.message for rec in caplog.records)
+        with XRaySegment():
+            with terracotta.profile.trace('dummy2'):
+                time.sleep(0.1)
+
+        for record in caplog.records:
+            assert record.levelname != 'ERROR'
+
+        # sanity check, recording without starting a segment should fail
+        func_to_trace()
+        assert any('cannot find the current segment' in rec.message for rec in caplog.records)
+
+    run_test()
 
 
 @pytest.mark.filterwarnings('ignore:pytest.PytestUnraisableExceptionWarning')
-@mock_xray_client
 def test_xray_exception(caplog):
-    from terracotta import update_settings
-    import terracotta.profile
+    from moto import mock_xray_client, XRaySegment
+    # use another closure so mock_xray_client isn't in global scope
 
-    update_settings(XRAY_PROFILE=True)
+    @mock_xray_client
+    def run_test():
+        from terracotta import update_settings
+        import terracotta.profile
 
-    with XRaySegment():
-        with pytest.raises(NotImplementedError):
-            with terracotta.profile.trace('dummy') as subsegment:
-                raise NotImplementedError('foo')
+        update_settings(XRAY_PROFILE=True)
 
-    assert len(subsegment.cause['exceptions']) == 1
-    assert subsegment.cause['exceptions'][0].message == 'foo'
+        with XRaySegment():
+            with pytest.raises(NotImplementedError):
+                with terracotta.profile.trace('dummy') as subsegment:
+                    raise NotImplementedError('foo')
+
+        assert len(subsegment.cause['exceptions']) == 1
+        assert subsegment.cause['exceptions'][0].message == 'foo'
+
+    run_test()
