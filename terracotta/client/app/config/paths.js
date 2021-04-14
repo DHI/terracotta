4565340c19827fa66dc54063af230288b46823b2
bertearazvan <bertea74@gmail.com>
2021-04-14 11:12:56 +0200

    change folder structure as it was before

--- terracotta/client/app/config/paths.js
+++ terracotta/client/app/config/paths.js
@@ -54,7 +54,7 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('../flask-server/static/react'),
+  appBuild: resolveApp('../static/react'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
