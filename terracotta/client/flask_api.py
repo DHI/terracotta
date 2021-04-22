fa8b92a2676b7aaba38459c87a2af43987aa7624
bertearazvan <bertea74@gmail.com>
2021-04-22 17:24:35 +0200

    fix spaces in flask_api

--- terracotta/client/flask_api.py
+++ terracotta/client/flask_api.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, current_app, Blueprint
 
 client_api = Blueprint('client_api', 'terracotta.client')
 
+
 @client_api.route('/', methods=['GET'])
 def get_map() -> Any:
     return render_template(
@@ -16,15 +17,3 @@ def create_app(hostname: str) -> Flask:
     client_app.config['terracotta_hostname'] = hostname
     client_app.register_blueprint(client_api)
     return client_app
-
-# client_api.run(debug=True)
-#
-# from flask import (Flask, render_template)
-
-# app = Flask("__main__")
-
-# @app.route("/")
-# def my_index():
-#     return render_template("index.html", flask_token="Hello   world")
-
-# app.run(debug=True)
