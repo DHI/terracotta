c936905660fbfabb8ced84587d82a739e37f9df0
Nicklas Boserup <nibo@dhigroup.com>
2022-01-31 10:17:28 +0100

    Rename accordingly to Driver refactor

--- terracotta/handlers/datasets.py
+++ terracotta/handlers/datasets.py
@@ -19,7 +19,7 @@ def datasets(some_keys: Mapping[str, Union[str, List[str]]] = None,
 
     with driver.connect():
         dataset_keys = driver.get_datasets(
-            where=some_keys, page=page, limit=limit
+            keys=some_keys, page=page, limit=limit
         ).keys()
         key_names = driver.key_names
 
