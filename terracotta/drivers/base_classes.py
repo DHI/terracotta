046720df73fe3731983b7c6fe5bd350a7623ac4c
Nicklas Boserup <nibo@dhigroup.com>
2022-02-01 17:01:21 +0100

    Remember the new/renamed files!

---
+++ terracotta/drivers/base_classes.py
@@ -0,0 +1,211 @@
+"""drivers/base_classes.py
+
+Base class for drivers.
+"""
+
+import contextlib
+import functools
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+from typing import (Any, Callable, Dict, List, Mapping, Optional, Sequence,
+                    Tuple, TypeVar, Union)
+
+KeysType = Mapping[str, str]
+MultiValueKeysType = Mapping[str, Union[str, List[str]]]
+Number = TypeVar('Number', int, float)
+T = TypeVar('T')
+
+
+def requires_connection(
+    fun: Callable[..., T] = None, *,
+    verify: bool = True
+) -> Union[Callable[..., T], functools.partial]:
+    if fun is None:
+        return functools.partial(requires_connection, verify=verify)
+
+    @functools.wraps(fun)
+    def inner(self: MetaStore, *args: Any, **kwargs: Any) -> T:
+        assert fun is not None
+        with self.connect(verify=verify):
+            return fun(self, *args, **kwargs)
+
+    return inner
+
+
+class MetaStore(ABC):
+    """Abstract base class for all Terracotta data backends.
+
+    Defines a common interface for all drivers.
+    """
+    _RESERVED_KEYS = ('limit', 'page')
+
+    @property
+    @abstractmethod
+    def db_version(self) -> str:
+        """Terracotta version used to create the database."""
+        pass
+
+    @property
+    @abstractmethod
+    def key_names(self) -> Tuple[str, ...]:
+        """Names of all keys defined by the database."""
+        pass
+
+    @abstractmethod
+    def __init__(self, url_or_path: str) -> None:
+        self.path = url_or_path
+
+    @classmethod
+    def _normalize_path(cls, path: str) -> str:
+        """Convert given path to normalized version (that can be used for caching)"""
+        return path
+
+    @abstractmethod
+    def create(self, keys: Sequence[str], *,
+               key_descriptions: Mapping[str, str] = None) -> None:
+        # Create a new, empty database (driver dependent)
+        pass
+
+    @abstractmethod
+    def connect(self, verify: bool = True) -> contextlib.AbstractContextManager:
+        """Context manager to connect to a given database and clean up on exit.
+
+        This allows you to pool interactions with the database to prevent possibly
+        expensive reconnects, or to roll back several interactions if one of them fails.
+
+        Arguments:
+
+            verify: Whether to verify the database (primarily its version) when connecting.
+                Should be `true` unless absolutely necessary, such as when instantiating the
+                database during creation of it.
+
+        Note:
+
+            Make sure to call :meth:`create` on a fresh database before using this method.
+
+        Example:
+
+            >>> import terracotta as tc
+            >>> driver = tc.get_driver('tc.sqlite')
+            >>> with driver.connect():
+            ...     for keys, dataset in datasets.items():
+            ...         # connection will be kept open between insert operations
+            ...         driver.insert(keys, dataset)
+
+        """
+        pass
+
+    @abstractmethod
+    def get_keys(self) -> OrderedDict:
+        """Get all known keys and their fulltext descriptions.
+
+        Returns:
+
+            An :class:`~collections.OrderedDict` in the form
+            ``{key_name: key_description}``
+
+        """
+        pass
+
+    @abstractmethod
+    def get_datasets(self, where: MultiValueKeysType = None,
+                     page: int = 0, limit: int = None) -> Dict[Tuple[str, ...], Any]:
+        # Get all known dataset key combinations matching the given constraints,
+        # and a handle to retrieve the data (driver dependent)
+        pass
+
+    @abstractmethod
+    def get_metadata(self, keys: KeysType) -> Optional[Dict[str, Any]]:
+        """Return all stored metadata for given keys.
+
+        Arguments:
+
+            keys: Keys of the requested dataset. Can either be given as a sequence of key values,
+                or as a mapping ``{key_name: key_value}``.
+
+        Returns:
+
+            A :class:`dict` with the values
+
+            - ``range``: global minimum and maximum value in dataset
+            - ``bounds``: physical bounds covered by dataset in latitude-longitude projection
+            - ``convex_hull``: GeoJSON shape specifying total data coverage in latitude-longitude
+              projection
+            - ``percentiles``: array of pre-computed percentiles from 1% through 99%
+            - ``mean``: global mean
+            - ``stdev``: global standard deviation
+            - ``metadata``: any additional client-relevant metadata
+
+        """
+        pass
+
+    @abstractmethod
+    def insert(self, keys: KeysType,
+               handle: Any, **kwargs: Any) -> None:
+        """Register a new dataset. Used to populate metadata database.
+
+        Arguments:
+
+            keys: Keys of the dataset. Can either be given as a sequence of key values, or
+                as a mapping ``{key_name: key_value}``.
+            handle: Handle to access dataset (driver dependent).
+
+        """
+        pass
+
+    @abstractmethod
+    def delete(self, keys: KeysType) -> None:
+        """Remove a dataset from the metadata database.
+
+        Arguments:
+
+            keys:  Keys of the dataset. Can either be given as a sequence of key values, or
+                as a mapping ``{key_name: key_value}``.
+
+        """
+        pass
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(\'{self.path}\')'
+
+
+class RasterStore(ABC):
+
+    @abstractmethod
+    # TODO: add accurate signature if mypy ever supports conditional return types
+    def get_raster_tile(self, handle: str, *,
+                        tile_bounds: Sequence[float] = None,
+                        tile_size: Sequence[int] = (256, 256),
+                        preserve_values: bool = False,
+                        asynchronous: bool = False) -> Any:
+        """Load a raster tile with given handle and bounds.
+
+        Arguments:
+
+            handle: Handle of the requested dataset.
+            tile_bounds: Physical bounds of the tile to read, in Web Mercator projection (EPSG3857).
+                Reads the whole dataset if not given.
+            tile_size: Shape of the output array to return. Must be two-dimensional.
+                Defaults to :attr:`~terracotta.config.TerracottaSettings.DEFAULT_TILE_SIZE`.
+            preserve_values: Whether to preserve exact numerical values (e.g. when reading
+                categorical data). Sets all interpolation to nearest neighbor.
+            asynchronous: If given, the tile will be read asynchronously in a separate thread.
+                This function will return immediately with a :class:`~concurrent.futures.Future`
+                that can be used to retrieve the result.
+
+        Returns:
+
+            Requested tile as :class:`~numpy.ma.MaskedArray` of shape ``tile_size`` if
+            ``asynchronous=False``, otherwise a :class:`~concurrent.futures.Future` containing
+            the result.
+
+        """
+        pass
+
+    @abstractmethod
+    def compute_metadata(self, handle: str, *,
+                         extra_metadata: Any = None,
+                         use_chunks: bool = None,
+                         max_shape: Sequence[int] = None) -> Dict[str, Any]:
+        # Compute metadata for a given input file (driver dependent)
+        pass
---
+++ terracotta/drivers/terracotta_driver.py
@@ -0,0 +1,164 @@
+"""drivers/terracotta_driver.py
+
+The driver to interact with.
+"""
+
+import contextlib
+from collections import OrderedDict
+from typing import (Any, Collection, Dict, Mapping, Sequence, Tuple, TypeVar,
+                    Union)
+
+import terracotta
+from terracotta import exceptions
+from terracotta.drivers.base_classes import (KeysType, MetaStore,
+                                             MultiValueKeysType, RasterStore,
+                                             requires_connection)
+
+ExtendedKeysType = Union[Sequence[str], KeysType]
+T = TypeVar('T')
+
+
+def only_element(iterable: Collection[T]) -> T:
+    if not iterable:
+        raise exceptions.DatasetNotFoundError('No dataset found')
+    assert len(iterable) == 1
+    return next(iter(iterable))
+
+
+class TerracottaDriver:
+
+    def __init__(self, metastore: MetaStore, rasterstore: RasterStore) -> None:
+        self.metastore = metastore
+        self.rasterstore = rasterstore
+
+        settings = terracotta.get_settings()
+        self.LAZY_LOADING_MAX_SHAPE: Tuple[int, int] = settings.LAZY_LOADING_MAX_SHAPE
+
+    @property
+    def db_version(self) -> str:
+        return self.metastore.db_version
+
+    @property
+    def key_names(self) -> Tuple[str, ...]:
+        return self.metastore.key_names
+
+    def create(self, keys: Sequence[str], *,
+               key_descriptions: Mapping[str, str] = None) -> None:
+        self.metastore.create(keys=keys, key_descriptions=key_descriptions)
+
+    def connect(self, verify: bool = True) -> contextlib.AbstractContextManager:
+        return self.metastore.connect(verify=verify)
+
+    @requires_connection
+    def get_keys(self) -> OrderedDict:
+        return self.metastore.get_keys()
+
+    @requires_connection
+    def get_datasets(self, keys: MultiValueKeysType = None,
+                     page: int = 0, limit: int = None) -> Dict[Tuple[str, ...], Any]:
+        return self.metastore.get_datasets(
+            where=keys,
+            page=page,
+            limit=limit
+        )
+
+    @requires_connection
+    def get_metadata(self, keys: ExtendedKeysType) -> Dict[str, Any]:
+        keys = self._standardize_keys(keys)
+
+        metadata = self.metastore.get_metadata(keys)
+
+        if metadata is None:
+            # metadata is not computed yet, trigger lazy loading
+            handle = only_element(self.get_datasets(keys).values())
+            metadata = self.compute_metadata(handle, max_shape=self.LAZY_LOADING_MAX_SHAPE)
+            self.insert(keys, handle, metadata=metadata)
+
+            # this is necessary to make the lazy loading tests pass...
+            metadata = self.metastore.get_metadata(keys)
+            assert metadata is not None
+
+        return metadata
+
+    @requires_connection
+    def insert(
+        self, keys: ExtendedKeysType,
+        handle: Any, *,
+        override_path: str = None,
+        metadata: Mapping[str, Any] = None,
+        skip_metadata: bool = False,
+        **kwargs: Any
+    ) -> None:
+        keys = self._standardize_keys(keys)
+
+        if metadata is None and not skip_metadata:
+            metadata = self.compute_metadata(handle)
+
+        self.metastore.insert(
+            keys=keys,
+            handle=override_path or handle,
+            metadata=metadata,
+            **kwargs
+        )
+
+    @requires_connection
+    def delete(self, keys: ExtendedKeysType) -> None:
+        keys = self._standardize_keys(keys)
+
+        self.metastore.delete(keys)
+
+    def get_raster_tile(self, keys: ExtendedKeysType, *,
+                        tile_bounds: Sequence[float] = None,
+                        tile_size: Sequence[int] = (256, 256),
+                        preserve_values: bool = False,
+                        asynchronous: bool = False) -> Any:
+        handle = only_element(self.get_datasets(keys).values())
+
+        return self.rasterstore.get_raster_tile(
+            handle=handle,
+            tile_bounds=tile_bounds,
+            tile_size=tile_size,
+            preserve_values=preserve_values,
+            asynchronous=asynchronous,
+        )
+
+    def compute_metadata(self, handle: str, *,
+                         extra_metadata: Any = None,
+                         use_chunks: bool = None,
+                         max_shape: Sequence[int] = None) -> Dict[str, Any]:
+        return self.rasterstore.compute_metadata(
+            handle=handle,
+            extra_metadata=extra_metadata,
+            use_chunks=use_chunks,
+            max_shape=max_shape,
+        )
+
+    def _standardize_keys(
+        self,
+        keys: ExtendedKeysType,
+        requires_all_keys: bool = True
+    ) -> KeysType:
+        if requires_all_keys and (keys is None or len(keys) != len(self.key_names)):
+            raise exceptions.InvalidKeyError(
+                f'Got wrong number of keys (available keys: {self.key_names})'
+            )
+
+        if isinstance(keys, Mapping):
+            keys = dict(keys.items())
+        elif isinstance(keys, Sequence):
+            keys = dict(zip(self.key_names, keys))
+        else:
+            raise exceptions.InvalidKeyError(
+                'Encountered unknown key type, expected Mapping or Sequence'
+            )
+
+        unknown_keys = set(keys) - set(self.key_names)
+        if unknown_keys:
+            raise exceptions.InvalidKeyError(
+                f'Encountered unrecognized keys {unknown_keys} (available keys: {self.key_names})'
+            )
+
+        return keys
+
+    def __repr__(self) -> str:
+        return self.metastore.__repr__()
