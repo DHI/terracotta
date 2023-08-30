71f2c2a4496d8c21342a37ffd22daf1c0ed2daed
Dion Häfner <dion.haefner@nbi.ku.dk>
2023-08-30 13:17:43 +0200

    fix migration recipe to v0.8; test migration more thoroughly (#316)

--- terracotta/migrations/v0_8.py
+++ terracotta/migrations/v0_8.py
@@ -4,5 +4,6 @@ down_version = (0, 7)
 upgrade_sql = [
     "CREATE TABLE key_names (key_name TEXT PRIMARY KEY, description TEXT, idx INTEGER UNIQUE)",
     "INSERT INTO key_names (key_name, description, idx) SELECT key, description, row_number() over (order by (select NULL)) FROM keys",
+    "ALTER TABLE datasets RENAME COLUMN filepath TO path",
     "UPDATE terracotta SET version='0.8.0'",
 ]
