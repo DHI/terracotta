19e2e771d9f8b1014a62724a183cb8a2f1d6dfdf
charalamm <charalamm@protonmail.com>
2023-09-08 12:52:31 +0200

    Add gunicorn

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-terracotta serve --allow-all-ips --port $TC_SERVER_PORT -d $TC_DRIVER_PATH
+
+# Validate that environment variables are set
+if [ -z "$TC_DRIVER_PATH" ]; then
+  echo "TC_DRIVER_PATH is not set. Exiting."
+  exit 1
+fi
+
+
+# Start the server
+gunicorn terracotta.server.app:app --bind 0.0.0.0:$TC_SERVER_PORT
