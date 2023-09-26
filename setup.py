af805d940bbcf0287cc19fbfa9bfc25f22da6ec5
Dion Häfner <dion.haefner@simulation.science>
2023-09-26 11:24:22 +0200

    :lipstick:

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Framework :: Flask",
         "Operating System :: Microsoft :: Windows :: Windows 10",
         "Operating System :: MacOS :: MacOS X",
