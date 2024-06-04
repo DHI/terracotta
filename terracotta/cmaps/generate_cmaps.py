b8a1b5321239921382ac208d90a8141584393dc1
Dion Häfner <dion.haefner@simulation.science>
2024-06-04 12:22:47 +0200

    use newer mpl.colormaps object

--- terracotta/cmaps/generate_cmaps.py
+++ terracotta/cmaps/generate_cmaps.py
@@ -4,11 +4,11 @@ A script to generate color map dumps from matplotlib.
 """
 
 import numpy as np
-import matplotlib.cm as cm
+import matploblib as mpl
 
 from terracotta.cmaps.get_cmaps import SUFFIX
 
-ALL_CMAPS = list(cm.cmaps_listed) + list(cm.datad)
+ALL_CMAPS = list(mpl.colormaps)
 ALL_CMAPS.extend([f"{cmap}_r" for cmap in ALL_CMAPS])
 NUM_VALS = 255
 
@@ -17,7 +17,7 @@ def generate_maps(out_folder: str) -> None:
     x = np.linspace(0, 1, NUM_VALS)
     for cmap in ALL_CMAPS:
         print(cmap)
-        cmap_fun = cm.get_cmap(cmap)
+        cmap_fun = mpl.colormaps[cmap]
         cmap_vals = cmap_fun(x)
         cmap_uint8 = (cmap_vals * 255).astype("uint8")
         np.save(f"{out_folder}/{cmap.lower()}{SUFFIX}", cmap_uint8)
