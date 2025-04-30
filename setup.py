5808695312aa160c8d002968c0e5d9f55ae66e7e
Dion Häfner <dion.haefner@simulation.science>
2025-04-30 10:07:58 +0200

    pin marshmallow to <4

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
         "color-operations",
         "flask",
         "flask_cors",
-        "marshmallow>=3.0.0",
+        "marshmallow>=3.0.0,<4",
         "mercantile",
         "numpy%s" % numpy_version,
         "pillow",
